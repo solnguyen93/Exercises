9f24c094c49389f509a7d21b9be7b56fbdf5d2a8
Sol_Nguyen <solnguyen93@gmail.com>
2023-03-22 22:14:46 -0700

    files creation

---
+++ letAndConstExercise.js
@@ -0,0 +1,15 @@
+/* Write an ES2015 Version */
+const PI = 3.14;
+// Quiz
+// What is the difference between var and let?
+//  var can be redeclared, reassign/update. let can't be redeclared, only reassign/update
+//
+// What is the difference between var and const?
+//  var can be redeclared, reassign/update. const is fixed.
+//
+// What is the difference between let and const?
+//  let can be reassign/update, const can NOT.
+//
+// What is hoisting?
+//  explaination on what JS compile first. Var declaration is always first even if the values are assigned later
+//
