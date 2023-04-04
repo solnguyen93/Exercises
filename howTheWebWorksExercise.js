1cb41d1faf6f55ee6044b7e19635cab75c066af1
Sol_Nguyen <solnguyen93@gmail.com>
2023-04-03 22:36:01 -0700

    howTheWebWorks Exercise

---
+++ howTheWebWorksExercise.js
@@ -0,0 +1,40 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////// PART ONE
+// What is HTTP?
+// HyperText Transfer Protocol, a set of rules/standard on how web browers ask/communicate for the information they need to load a website.
+
+// What is a URL?
+//Uniform Resource Locator, an address of a unique resource on the web
+
+// What is DNS?
+//Domain Name System, take a hostname and turn it into an IP address.
+
+// What is a query string?
+//end portion of URL that passes information/data on to the web server
+
+// What are two HTTP verbs and how are they different?
+//GET and POST, GET requests doesn't change server data whereas POST does
+
+// What is an HTTP request?
+//A request made by the client to the server to access a resource
+
+// What is an HTTP response?
+//A response made by a server to the client
+
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+//Additional content, data, resource, etc. transferred between client and server
+//request: allow, server, range, age, expirres
+//response: set-cooki, content-type, keep-alive
+
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+//the browser search for the IP Address associated with the hostname in the DNS
+//check browser cache -> local cache -> router cache -> ISP cache
+//Connect with the server for data transmission
+//sends an HTTP request to the server
+//containning method, HTTP protocol version, resource URL, and headers.
+//server provesses the request and sends back a response
+//containing requested web page/data, HTTP protocol version, status code, headers, a body
+//browser renders the request data
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////// PART TWO
