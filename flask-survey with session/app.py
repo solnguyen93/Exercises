7c0729b721fb6f30973badddb54fedd4140baf2d
Sol_Nguyen <solnguyen93@gmail.com>
2023-05-24 15:00:24 -0700

    flask-survey with session

--- flask-survey with session/app.py
+++ flask-survey with session/app.py
@@ -7,58 +7,51 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key_here'
 debug = DebugToolbarExtension(app) 
 
-@app.route('/', methods=["GET","POST"]) # Home page
+@app.route('/', methods=["GET","POST"])
 def root():
-    """Show the title of the survey, the instructions, and a button to start the survey"""
-    if request.method == 'POST':
+    """
+    On home page(GET request for data) then show the title of the survey, the instructions, and a button to start the survey.
+    When start button is clicked (POST) -> make cookie session to store data and redirect to first question (/question/0)
+    """
+    if request.method == "POST":
         session["responses"] = []
-        return redirect('/questions/0')  # Redirect to the first question
+        return redirect('/questions/0') 
 
     return render_template('home.html', satisfaction_survey=satisfaction_survey) 
 
-@app.route('/questions/<int:question_index>', methods=["GET","POST"]) 
+@app.route('/questions/<int:question_index>') 
 def question(question_index):
-    print(len(session.get("responses", [])))
-    print(question_index)
     """
     when visiting /question/# it triggers a GET request to retrieve and display(using question.html) the current question based on the question_index parameter.
     When the form submits(answer question) within question.html it triggers a POST request to /answer
+    Condition to prevent accessing questions before start and skipping ahead question
     """
-    # prevent access to /question/0 in url (must use start button)
-    if request.method == 'GET' and not session.get("responses"):
-        flash('Please start the survey first.')
+    if session.get("responses") is None:    # trying to access questions before start
         return redirect('/')
-    # prevent skipping question using url
-    if question_index != len(session.get("responses", [])):
-        flash('Please answer the questions sequentially')
-        return redirect(f'/questions/{len(session.get("responses", []))}')
-
-    return render_template('question.html', question=satisfaction_survey.questions[question_index], index=question_index)
+    if len(session.get("responses")) !=  question_index:    # trying to access questions out of order
+        return redirect(f'/questions/{len(session.get("responses"))}')  
+    else:
+        return render_template('question.html', question=satisfaction_survey.questions[question_index], index=question_index)
 
 @app.route('/answer', methods=["POST"])
 def answer():
     """
     Data(response/answer) send here as a POST request from question.html form.
-    Store the data in responses(list), check if all questions are completed then redirect to /complete page.
+    Store the data in session['responses'], check if all questions are completed then redirect to /complete page.
     Redirect using GET request to next /question/# page
     """
-    question_index = int(request.form['index'])
-    response = request.form['answer']
-    
-    # Initialize the responses list in the session if it doesn't exist
-    session.setdefault("responses", [])
-    session["responses"].append(response)
+    response = request.form['answer']   # retrieves the value(answer) of the form and set it to response
+    responses = session["responses"]    # retrieves session data and set it to responses
+    responses.append(response)  # add response to responses
+    session["responses"] = responses    # updates the value stored in the session
 
-    if question_index == (len(satisfaction_survey.questions) - 1):
+    if len(session["responses"]) == (len(satisfaction_survey.questions)):   # answered all questions
         return redirect('/complete')
-    else:
-        return redirect(f'/questions/{question_index + 1}')
+
+    question_index = int(request.form['index'])
+    return redirect(f'/questions/{question_index + 1}')     # redirect to next question
 
 @app.route('/complete')
 def complete():
     """Show the survey completion page"""
-    # prevent access to complete page until all questions are completed
-    if len(session.get("responses", [])) != len(satisfaction_survey.questions):
-        flash('Please answer all the questions.')
-        return redirect(f'/questions/{len(session.get("responses", []))}')
-   
+    return render_template('complete.html')
